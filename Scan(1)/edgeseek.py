d27624ea8d4b8cc8e5964046b140cbfbb6d546cb
dreamkill <202131106020089@stu.hubu.edu.cn>
2025-08-12 14:16:46 +0800

    Add files via upload

---
+++ Scan(1)/edgeseek.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+# 读取图片
+testimage = cv2.imread('')
+
+
+def edgeseek(img):
+    # 边缘检测代码
+
+    cv2.imwrite('result.jpg',img)
+    return
+
+
+if __name__ == '__main__':
+    edgeseek(edgeseek(testimage))
